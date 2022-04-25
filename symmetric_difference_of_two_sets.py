72f8353fbbc1494be80dbe957d6811113b1a419e
Dipesh patidar <94466488+dipeshpatidarjeeran@users.noreply.github.com>
2022-04-25 18:25:30 +0530

    Add files via upload

---
+++ symmetric_difference_of_two_sets.py
@@ -0,0 +1,6 @@
+s1={5,3,2,1}
+s2={1,4,3,6,8}
+print("set1 is :-",s1)
+print("set2 is:-",s2)
+print("symmetric_difference of two sets:-",s1^s2)
+print("symmetric_difference of two sets:-",s1.symmetric_difference(s2))
